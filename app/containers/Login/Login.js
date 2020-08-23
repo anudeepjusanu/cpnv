a4239cbea5601073c76c0c250da6edfce77cd5ea
arun <arunkumark@wavelabs.ai>
2020-08-23 09:10:29 +0530

    catcha

--- app/containers/Login/Login.js
+++ app/containers/Login/Login.js
@@ -5,18 +5,50 @@
  *
  */
 
-import React from 'react';
+import React, { Component, useState } from 'react';
 import Button from '@material-ui/core/Button';
+import Recaptcha from 'react-recaptcha';
+import history from "utils/history";
 
 function Login(props) {
-  return (
-    <React.Fragment>
-      <h1>HELLO FROM CEPHEID</h1>
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
-    </React.Fragment>
-  );
+ 
+  const [isVerified, setIsVerified] = useState(false)
+
+  const recaptchaLoaded = () => {
+    console.log('capcha successfully loaded');
+  }
+
+  const handleSubscribe = () => {
+    if (isVerified) {
+      history.push(`/intakeForm`);
+    } else {
+      alert('Please verify that you are a human!');
+    }
+  }
+
+  const verifyCallback = (response) => {
+    if (response) {
+      setIsVerified(true);
+    }
+  }
+
+    return (
+        <div className="App-intro">
+          <input type="text" placeholder="email@company.com" />
+
+          <Button
+            className="convert"
+            onClick={handleSubscribe}
+          >Subscribe</Button>
+
+          <Recaptcha
+            sitekey="6LfAQcIZAAAAAKhP-NoTdl4GzwyBXenhjKwoxXOv"
+            render="explicit"
+            onloadCallback={recaptchaLoaded}
+            verifyCallback={verifyCallback}
+          />
+        </div>
+    );
 }
 
 export default Login;
