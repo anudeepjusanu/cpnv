c69fd78e917a8ea94acaab4c4a2f3e67fcc87637
Anudeep D <anudeepd@nbsotech.com>
2020-08-30 16:25:05 +0530

    routes changes

--- app/components/HrbpRoute/HrbpRoute.js
+++ app/components/HrbpRoute/HrbpRoute.js
@@ -1,24 +1,42 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { Layout } from 'containers/Layout';
 // import { HrbpLayout } from 'containers/HrbpLayout';
-import HrbpLayout from '../../containers/HrbpLayout/HrbpLayout';
+import HrbpLayout from 'containers/HrbpLayout/HrbpLayout';
 
-const HrbpRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={props =>
-      localStorage.getItem('user') &&
-      JSON.parse(localStorage.getItem('user')).apiKey ? (
-        <Redirect to={{ pathname: '/', state: { from: props.location } }} />
-      ) : (
-        <HrbpLayout {...props} {...rest}>
-          <Component {...props} />
-        </HrbpLayout>
-      )
-    }
-  />
-);
+const HrbpRoute = ({ component: Component, ...rest }) => {
+  console.log(rest.config.role);
+  const roles = {
+    HRM: 'hrm',
+    HRBP: 'hrbp',
+    CRT: 'crt',
+  };
+  const user = JSON.parse(localStorage.getItem('user'));
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        user && user.token ? (
+          user.role === rest.config.role ? (
+            <HrbpLayout {...props} {...rest}>
+              <Component {...props} />
+            </HrbpLayout>
+          ) : (
+            <Redirect
+              to={{
+                pathname: `/${roles[user.role]}/caseList`,
+                state: { from: props.location },
+              }}
+            />
+          )
+        ) : (
+          <Redirect
+            to={{ pathname: '/userLogin', state: { from: props.location } }}
+          />
+        )
+      }
+    />
+  );
+};
 
 export default HrbpRoute;
