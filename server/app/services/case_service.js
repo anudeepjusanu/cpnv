667e03025e60c9e67494c44a6b9c854d80f4f61d
L B Sastry <lbsastry@wavelabs.ai>
2020-09-04 22:17:01 +0530

    area adedd

--- server/app/services/case_service.js
+++ server/app/services/case_service.js
@@ -41,11 +41,11 @@ service.getCase = async (caseId, email) => {
     };
     if (user_info && user_info.role) {
         if (user_info.role == 'CRT') {
-            var case_info = await coreService.query(`SELECT c.case_id, c.department_id, d.department_name,
+            var case_info = await coreService.query(`SELECT c.case_id,
             c.case_status, c.reason, c.exposure_date, c.exposure_describe, c.is_positive_diagnosis, c.area,
             c.diagnosis_received_date, c.diagnosis_test_date, c.symptoms_began_date, c.symptoms_respiratory, 
             c.have_consult_doctor, c.consult_date, c.company_buildings, c.additional_info, c.review_additional_info, c.created_on
-            FROM tbl_cases c LEFT JOIN tbl_departments d ON c.department_id = d.department_id WHERE case_id = '${caseId}' `);
+            FROM tbl_cases c WHERE case_id = '${caseId}' `);
             case_info = case_info[0] ? case_info[0] : {};
         } else {
             var case_info = await coreService.query(
